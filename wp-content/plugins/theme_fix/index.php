af7fddf00c867798c288d832c2dfbda2beb80c86
Romez <Romez1990@gmail.com>
2019-12-18 22:28:16 +0300

    Refactor plugin

--- wp-content/plugins/theme_fix/index.php
+++ wp-content/plugins/theme_fix/index.php
@@ -5,37 +5,31 @@
  * Author: Romez1990
  */
 
-add_action('init', 'fix_theme');
+add_action('wp_enqueue_scripts', 'fix_theme', 50);
 function fix_theme() {
-	fix_theme_error_message();
-	add_address_and_phone();
-}
-function fix_theme_error_message() {
 	?>
     <script>
         document.addEventListener('DOMContentLoaded', () => {
             const $ = jQuery;
+            fix_theme_error_message($);
+            add_address_and_phone($);
+        });
+
+        function fix_theme_error_message($) {
             $('.main-header .row div:nth-child(2) b, .main-header .row div:nth-child(2) br').detach();
             const form = $('.main-header .row div:nth-child(2) form');
             const col = $('.main-header .row div:nth-child(2)');
             col.text('');
             col.append(form);
-        });
-    </script>
-	<?php
-}
-function add_address_and_phone() {
-	?>
-    <script>
-        document.addEventListener('DOMContentLoaded', () => {
-            const $ = jQuery;
+        }
+
+        function add_address_and_phone($) {
             const siteDescription = $('.site-description');
             const address = 'ул. 30 лет Победы, 18а';
             const phone = 'тел./факс: 8 (8639) 27 53 79';
             siteDescription.html(`${siteDescription.text()
             }<br>${address}<br>${phone}`)
-        });
+        }
     </script>
 	<?php
-
 }
